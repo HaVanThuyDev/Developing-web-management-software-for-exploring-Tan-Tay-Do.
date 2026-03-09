7fa1a553aadc900eaf4f4ae2bf17e09421c79af8
Hà văn thủy <168401722+HaVanThuyDev@users.noreply.github.com>
2026-03-10 02:28:18 +0700

    first commit

---
+++ src/main/java/com/example/da_tantaydo/DaTanTayDoApplication.java
@@ -0,0 +1,13 @@
+package com.example.da_tantaydo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaTanTayDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaTanTayDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/da_tantaydo/DaTanTayDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.da_tantaydo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaTanTayDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
